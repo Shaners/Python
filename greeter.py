0a527669b845793a8b5efa8f4e79c28ce9f3c88f
Shane Lister <shane@atramentous.com>
2018-04-20 10:37:13 -0700

    Greeter added

---
+++ greeter.py
@@ -0,0 +1,8 @@
+# Greeter
+# This will prompt the user to provide their name and then greet them
+
+# Prompt string input from user
+userInput = raw_input("Please enter your name:\n> ")
+
+# Greet user
+print "Hello %s! It is very nice to meet you!" % userInput
