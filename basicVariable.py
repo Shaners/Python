176f84f9af54aa98882da2743638fc7646ab7859
Shane Lister <shane@atramentous.com>
2018-04-26 08:48:12 -0700

    Added alternative preferred print formatting

--- basicVariable.py
+++ basicVariable.py
@@ -7,4 +7,4 @@ fruit_consumed_per_person_per_day = 1
 print("There are", people, "people in the house.")
 print("We have", fruit_count, "{}(s).".format(fruit))
 print(people * fruit_consumed_per_person_per_day, fruit, "will be consumed per day.")
-print("We will run out of", fruit, "in", fruit_count / (people * fruit_consumed_per_person_per_day), "day(s)!")
+print(f"We will run out of {fruit} in {fruit_count / (people * fruit_consumed_per_person_per_day)} day(s)!")
